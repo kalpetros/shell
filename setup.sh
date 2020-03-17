d807f476d03754b6cb2b8e07a0cb9b93bb47ba5e
Petros Kalogiannakis <github@petroskal.com>
2020-03-17 02:43:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,51 @@
 #!/bin/sh
 
+GREEN=$(printf '\033[32m')
+DEFAULT=$(printf '\033[m')
+
 command_exists() {
     command -v "${@}" >/dev/null 2>&1
 }
 
+setup_bash() {
+    echo "${GREEN}Setting up bash...${DEFAULT}"
+    mkdir ~/.bash_config
+    cp .bash_aliases ~/
+    cp git-prompt.sh  ~/.bash_config/
+    cp git-completion.bash ~/.bash_config/
+    echo "${GREEN}Finished bash setup${DEFAULT}"
+}
+
+setup_shell() {
+    if [ $(basename "$SHELL") = "zsh" ]; then
+	echo "${GREEN}zsh is already your default shell${DEFAULT}"
+	return
+    fi
+
+    echo "${GREEN}Making zsh your default shell..."
+    chsh -s $(which zsh)
+}
+
 setup_ohmyzsh() {
-    echo "Setting up oh-my-zsh"
+    echo "${GREEN}Setting up oh-my-zsh${DEFAULT}"
+
+    if ! command_exists curl; then
+	echo "${GREEN}curl is not installed. Please install curl and then run the script again.${DEFAULT}"
+	exit 1
+    fi
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+    echo "${GREEN}Finished oh-my-zsh setup${DEFAULT}"
+}
+
+setup_zshrc() {
+    echo "${GREEN}Restoring .zshrc...${DEFAULT}"
+    cp .zshrc ~/
 }
 
 setup_zsh() {
-    DEFAULT_SHELL=$(echo $SHELL)
-
-    echo "Setting up zsh..."
+    echo "${GREEN}Setting up zsh...${DEFAULT}"
 
     if ! command_exists zsh; then
 	echo "Installing..."
@@ -19,50 +53,39 @@ setup_zsh() {
 	echo "Successfully installed $(zsh --version)"
     fi
 
-    if [ $DEFAULT_SHELL != "/bin/zsh" ]; then
-	echo "Making zsh your default shell..."
-	chsh -s $(which zsh)
-    fi
-
+    setup_shell
     setup_ohmyzsh
-
-    echo "Restoring .zshrc..."
-    cp .zshrc ~/.zshrc
+    setup_zshrc
     
-    echo "zsh setup is complete! Log out from your user session and log back in to use zsh."
+    echo "${GREEN}Finished zsh setup${DEFAULT}"
+    echo "${GREEN}Log out from your user session and log back in to use zsh.${DEFAULT}"
 }
 
 setup_tmux() {
-    echo "Setting up tmux..."
+    echo "${GREEN}Setting up tmux...${DEFAULT}"
 
     if ! command_exists tmux; then
-	echo "Installing..."
+	echo "${GREEN}Installing...${DEFAULT}"
 	sudo apt-get install tmux -qq -y
-	echo "Successfully installed $(tmux -V)"
+	echo "${GREEN}Successfully installed $(tmux -V)${DEFAULT}"
     fi
 
-    echo "Do you want to start tmux automatically on new shell sessions? [y/n]"
-    read ANSWER
-
-    if [ $ANSWER = y ]; then
-	echo "Yes"
-    fi
-
-    echo "tmux setup is complete."
+    echo "${GREEN}tmux setup is complete.${DEFAULT}"
 }
 
 setup_tmuxinator() {
-    echo "Setting up tmuxinator..."
+    echo "${GREEN}Setting up tmuxinator...${DEFAULT}"
 
     if ! command_exists tmuxinator; then
-	echo "Installing..."
+	echo "${GREEN}Installing...${DEFAULT}"
 	sudo apt-get install tmuxinator -qq -y
-	echo "Successfully installed tmuxinator"
+	echo "${GREEN}Successfully installed tmuxinator${DEFAULT}"
     fi
 
-    echo "tmuxinator setup is complete."
+    echo "${GREEN}Finished tmuxinator setup${DEFAULT}"
 }
 
+setup_bash
 setup_zsh
 setup_tmux
 setup_tmuxinator
